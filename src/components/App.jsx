c169beb42b828d09008ba44b60b198f83fc1a6f9
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-03-09 16:35:54 +0100

    refactored code with redux/toolkit and redux-persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
+import Section from './section/Section';
+import Form from './form/Form';
+import ContactList from './contactList/ContactList';
+import { eachWordWithCapitalLetter } from '../utils';
+import { nanoid } from 'nanoid';
+import Filter from './filter/Filter';
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact } from '../store/ContactsSlice';
+
+const App = () => {
+  const contactsArr = useSelector(state => state.contacts.contactsArr);
+  const filter = useSelector(state => state.contacts.filter);
+
+  const dispatch = useDispatch();
+
+  const createConatct = item => {
+    if (
+      contactsArr.some(
+        contact => contact.name.toLowerCase() === item.name.toLowerCase()
+      )
+    ) {
+      alert(`${item.name} is already exist!`);
+      return;
+    }
+
+    const optimiseItemData = {
+      name: eachWordWithCapitalLetter(item.name),
+      number: item.number,
+      id: nanoid(),
+    };
+
+    dispatch(addContact(optimiseItemData));
+  };
+
+  const filteredContacts = contactsArr.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section>
+        <Form createConatct={createConatct} />
+      </Section>
+      <Section>
+        <Filter />
+        <ContactList contacts={filteredContacts} />
+      </Section>
+    </>
   );
 };
+
+export default App;
